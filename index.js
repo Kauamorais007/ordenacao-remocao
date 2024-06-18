86b30ec11c64bb375c3273189be6272aa52e8c4e
Kauã Morais <kaua.morais22@escola.pr.gov.br>
2024-06-18 08:51:34 -0300

    refactor: remove e ordena numeros

---
+++ index.js
@@ -0,0 +1,18 @@
+let numeros = [4, 3, 1, 5, 5, 7, 2, -47, 2001];
+
+for(let i = 0; i < numeros.length; i++) {
+    console.log('Primeiro laço', numeros[i]);
+    for(let j = i + 1; j < numeros.length; j++) {
+        console.log('Segundo laço', numeros[j]);
+        if (numeros[i] > numeros[j]) {
+            let aux = numeros[i];
+            numeros[i] = numeros[j];
+            numeros[j] = aux;
+        } else if (numeros[i] == numeros[j]) {
+            numeros.splice(j, 1)
+            j--
+        }
+    }
+}
+
+console.log('Array ordenado:', numeros);
